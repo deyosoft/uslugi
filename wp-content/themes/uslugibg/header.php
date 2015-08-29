0dd2d65cf694de380a26b66195b469463fdf4884
deyosoft <deyosoft@gmail.com>
2015-08-29 08:18:20 +0300

    Started work on custom theme

---
+++ wp-content/themes/uslugibg/header.php
@@ -0,0 +1,14 @@
+<!Doctype html>
+<html>
+<head>
+</head>
+<body>
+
+<div id="menu">
+	<ul>
+		<li><a href="#">Log in</a></li>
+		<li><a href="#">Register</a></li>
+		<li><a href="#">Rate</a></li>
+		<li><a href="#">About</a></li>
+	</ul>
+</div>
